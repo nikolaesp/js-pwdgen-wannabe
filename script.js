636bfb6814502079107b46367b5ae9700f4d2b24
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-08 15:30:01 +0100

    provaxjavasc

---
+++ script.js
@@ -0,0 +1,4 @@
+let nome = prompt("Inserisci il nome");
+let cognome = prompt("Inserisci il cognome");
+let colorepref = prompt("Inserisci colore preferita");
+console.log(nome+cognome+colorepref+"21");
