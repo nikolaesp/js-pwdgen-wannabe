08c9053ae55604d39daeadd51f068d7f6e2d6eb3
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-08 15:38:57 +0100

    js devisione numeri

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let nome = prompt("Inserisci il nome");
 let cognome = prompt("Inserisci il cognome");
 let colorepref = prompt("Inserisci colore preferita");
-console.log(nome+cognome+colorepref+"21");
+let num1 = prompt("Inserisci numero per devidere");
+let num2 = prompt("Inserisci numero che devide");
+let dev = parseInt(num1)/parseInt(num2);
+console.log(nome+cognome+colorepref+dev);
